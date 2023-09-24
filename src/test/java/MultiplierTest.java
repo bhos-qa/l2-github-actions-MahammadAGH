5d8197675aa3d3770dae4b25542c64dfb79cbc6d
azizahamadov <mehemmedaghakisiyev@gmail.com>
2023-09-24 18:56:13 +0400

    changed code

---
+++ src/test/java/MultiplierTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MultiplierTest {
+    @Test
+    public void testMultiply() {
+        int result = Multiplier.multiply(3, 4); // Replace with the numbers you want to test
+        assertEquals(12, result); // Replace with the expected result
+    }
+}
